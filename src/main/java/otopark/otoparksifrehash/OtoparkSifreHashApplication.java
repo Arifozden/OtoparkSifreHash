c0fb197ba2e7e36e39f048a7b9ea5e7165c58df9
Arifozden <111346692+Arifozden@users.noreply.github.com>
2023-05-20 22:19:42 +0200

    Initial commit

---
+++ src/main/java/otopark/otoparksifrehash/OtoparkSifreHashApplication.java
@@ -0,0 +1,13 @@
+package otopark.otoparksifrehash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtoparkSifreHashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtoparkSifreHashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/otopark/otoparksifrehash/OtoparkSifreHashApplicationTests.java
@@ -0,0 +1,13 @@
+package otopark.otoparksifrehash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtoparkSifreHashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
